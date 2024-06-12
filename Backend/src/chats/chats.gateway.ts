411e2574fa929a6d089dfd0f3a9700bc84410fa8
Taskeenhaider509 <taskeen_haider_intern@netixsol.com>
2024-06-12 11:58:12 +0500

    Sockets port change

--- Backend/src/chats/chats.gateway.ts
+++ Backend/src/chats/chats.gateway.ts
@@ -12,7 +12,7 @@ import { UseGuards } from '@nestjs/common';
 import { WsJwtAuthGuard } from 'src/config/guard/ws-jwt-auth.guard';
 import { wsAuthMiddleware } from 'src/config/middleware/ws-auth.middleware';
 
-@WebSocketGateway({
+@WebSocketGateway(800, {
   cors: {
     origin: '*',
     credentials: true,
